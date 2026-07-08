c04c08e233c2a92dad51c79d7b56206bc9c0287d
John Maar <johntylermaar@gmail.com>
2026-07-08 12:07:07 +0200

    reorganized the repo into 4 folders on category theory the function language the list language and everything else.
     I also split InsertionSort.lean into two files, the language spec in ListLanguage.Basic and the insertion sort specific tactics in ListLanguage.InsertionSort

--- DeductiveVericoding.lean
+++ DeductiveVericoding.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `DeductiveVericoding` library.
 -- Import modules here that should be built as part of the library.
-import DeductiveVericoding.Basic
+import DeductiveVericoding.ListLanguage.Basic

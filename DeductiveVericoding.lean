fd2013213a0b87d070fc1752d74ff3f8b49311ce
John Maar <johntylermaar@gmail.com>
2026-07-10 17:26:46 +0200

    Updated the root module

--- DeductiveVericoding.lean
+++ DeductiveVericoding.lean
@@ -1,3 +1,9 @@
 -- This module serves as the root of the `DeductiveVericoding` library.
 -- Import modules here that should be built as part of the library.
 import DeductiveVericoding.ListLanguage.Basic
+import DeductiveVericoding.ListLanguage.InsertionSort
+import DeductiveVericoding.ListLanguage.Problems
+import DeductiveVericoding.ListLanguage.Parametrized.Basic
+import DeductiveVericoding.ListLanguage.Parametrized.Tactics
+import DeductiveVericoding.ListLanguage.Parametrized.Translation
+import DeductiveVericoding.ListLanguage.Parametrized.Problems

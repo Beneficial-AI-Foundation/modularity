6aefcf90df2e7aade931fd49dd376f8a6fd0bb61
John Maar <johntylermaar@gmail.com>
2026-07-13 15:05:31 +0200

    some reorganization and renaming

--- DeductiveVericoding/ListLanguage/Parametrized/Problems.lean
+++ DeductiveVericoding/ListLanguage/Parametrized/Problems.lean
@@ -23,8 +23,8 @@ def AppendParameterSolution : AppendParameterProblem := by
   introP
   listRecP
   · apply ConsPTactic
-    · exact ParPTactic [Tpe.nat] Tpe.nat 0
-    apply NilPTactic'
+    · apply ParPTactic
+    apply NilPTactic
   · simp
     pushpreP
     apply ConsPTactic
@@ -38,7 +38,7 @@ abbrev ReverseProblem := ImplP []  (.arrow .list .list) (fun _ f => ∀ l, f l =
 
 def ReverseSolution : ReverseProblem := by
   listRecP
-  · apply NilPTactic'
+  · apply NilPTactic
   · simp
     pushpreP
     apply AppPTactic [Tpe.nat, Tpe.list, Tpe.list] .list .list _ (fun env l out => out = l.append [env.getT 0 Tpe.nat])
@@ -56,12 +56,16 @@ def ReverseSolution : ReverseProblem := by
 #eval Trm.pretty (toClosed ReverseSolution.code)
 -- "listRec((λ x0 : Unit => []), (λ x1 : Nat => (λ x2 : List => (λ x3 : List => listRec((λ x4 : Unit => x1 :: []), (λ x5 : Nat => (λ x6 : List => (λ x7 : List => x5 :: x7))))(x3)))))"
 
-/-- Regression test for the `introP` macro: it infers `s`, `t` and the residual condition
-    from the goal, including the de Bruijn shift when introducing under an existing parameter.
-    Note that `introP` is invoked with no explicit arguments at all. -/
-example : ImplP [] (.arrow .nat (.arrow .list .list)) (fun _ f => ∀ n, ∀ l, f n l = n :: l) := by
-  introP -- infers s = .nat, t = .arrow .list .list, Cond = fun e out => ∀ l, out l = e.getT 0 .nat :: l
-  introP -- infers s = .list, t = .list, shifting the earlier lookup to `e.getT 1 .nat`
+abbrev AppendListProblem := ImplP [] (.arrow .list (.arrow .list .list)) (fun _ f ↦ ∀ l1, ∀ l2, f l1 l2 = l2.append l1)
+
+def AppendListSolution : AppendListProblem := by
+  introP
+  listRecP
+  · apply ParPTactic
+  simp
+  pushpreP
   apply ConsPTactic
   · apply ParPTactic
   apply ParPTactic
+
+#eval Trm.pretty (toClosed AppendListSolution.code)
